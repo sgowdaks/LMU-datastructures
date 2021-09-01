dc5419391b801cab2ed1b7362bebbf6ff29bf99a
c7jansky <70697018+c7jansky@users.noreply.github.com>
2021-09-01 15:04:24 -0700

    Add files via upload
    program that prompts you to say your name followed by a printed response to say hello to you using your name

---
+++ Classwork/Classwork01/SayHello.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class SayHello {
+    public static void main( String args[] ){
+        
+        System.out.println("Enter your name.");
+
+        Scanner myInput = new Scanner( System.in );
+        String inputName = myInput.nextLine();
+
+        System.out.println( "Hello, " + inputName );
+
+    }
+}
