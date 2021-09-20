6f3e385770732cc832a15faae03bd8710242b161
Shivani Gowda KS <56445524+sgowdaks@users.noreply.github.com>
2021-09-20 13:50:35 -0700

    Update TargetFile.java

--- Classwork/Classwork02/copiersupport/TargetFile.java
+++ Classwork/Classwork02/copiersupport/TargetFile.java
@@ -1,1 +1,24 @@
+package copiersupport;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+
+public class TargetFile{
+
+	public String name;
+	public String line;
+
+	public TargetFile(String name, String everything){
+		this.name = name;
+		this.line = everything;
+	}
+
+	public void writeAll() throws Exception {
+		//java.io.FileWriter out = null;
+		 try (FileWriter fw = new FileWriter(this.name + ".copy")) {
+		    fw.write(line);
+	}
+	}
+
+}
