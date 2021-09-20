22237d4bd3ff8979c4ce5068efa1616c1cea1038
Shivani Gowda KS <56445524+sgowdaks@users.noreply.github.com>
2021-09-20 13:51:11 -0700

    Update MyFileCopier.java

--- Classwork/Classwork02/mainfile/MyFileCopier.java
+++ Classwork/Classwork02/mainfile/MyFileCopier.java
@@ -1,1 +1,19 @@
+package mainfile;
+import java.util.Scanner;
+import copiersupport.SourceFile;
 
+
+public class MyFileCopier {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Please provide the file name");
+		Scanner myInput = new Scanner( System.in );
+		String Name = myInput.nextLine();
+		SourceFile input = new SourceFile();
+		input.setName(Name);
+		
+
+	}
+
+}
