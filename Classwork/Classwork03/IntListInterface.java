d851045ee422fe8262d2148839c15a2a2ea26d55
Shivani Gowda KS <56445524+sgowdaks@users.noreply.github.com>
2021-09-21 09:14:34 -0700

    Update IntListInterface.java

--- Classwork/Classwork03/IntListInterface.java
+++ Classwork/Classwork03/IntListInterface.java
@@ -1,1 +1,8 @@
+package edu.lmu.cmsi2120.hw3;
 
+public interface IntListInterface {
+      int getValueAtIndex( int index );
+      boolean append( int valueToAdd );
+      boolean insertValueAtIndex( int value, int index );
+      int removeValueAtIndex( int index );
+    }
