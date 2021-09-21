8ac29502679d12cd011a86b22fb26de85d1e597c
Shivani Gowda KS <56445524+sgowdaks@users.noreply.github.com>
2021-09-21 12:18:06 -0700

    Update HighArrayApp.java

--- Homework/Homework01/HighArrayApp.java
+++ Homework/Homework01/HighArrayApp.java
@@ -1,1 +1,38 @@
+package edu.lmu.cmsi2120.HomeWork3;
+
+class HighArrayApp
+  {
+ public static void main(String[] args)
+ {
+int maxSize = 100;
+HighArray arr;
+arr = new HighArray(maxSize);
+arr.insert(77);
+arr.insert(99);
+arr.insert(88);
+arr.insert(44);
+arr.insert(55);
+arr.insert(22);
+arr.insert(66);
+arr.insert(88);
+arr.insert(11);
+arr.insert(00);
+arr.insert(66);
+arr.insert(33);
+arr.display();
+int searchKey = 35;
+if( arr.find(searchKey) )
+  System.out.println("Found " + searchKey);
+else
+  System.out.println("Can’t find " + searchKey);
+arr.delete(00);
+arr.delete(55);
+arr.delete(99);
+arr.display();
+long max = arr.getMax();
+System.out.println("Maximum Number in the array is "+max);
+arr.noDups();
+arr.display();
+}
+}
 
