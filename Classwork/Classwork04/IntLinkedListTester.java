ddda962626b01abfa0b058d2377a61c7c9e0218f
Shivani Gowda KS <56445524+sgowdaks@users.noreply.github.com>
2021-09-27 11:01:17 -0700

    Create IntLinkedListTester.java

---
+++ Classwork/Classwork04/IntLinkedListTester.java
@@ -0,0 +1,37 @@
+package edu.lmu.cmsi2120.ClassWork4;
+
+public class IntLinkedListTester {
+
+      public static void main( String[] args ) {
+         IntLinkedList myList = new IntLinkedList();
+         myList.prepend( 23 );
+         myList.prepend( 19 );
+         myList.prepend( 17 );
+         myList.prepend( 13 );
+         myList.prepend( 11 );
+         myList.prepend(  7 );
+         myList.prepend(  5 );
+         myList.prepend(  3 );
+         myList.prepend(  2 );
+         myList.insertAt(100,4);
+         int val = myList.removeAt(4);
+         IntLinkedList.Iterator myIt = myList.getIteratorAt( 0 );
+         System.out.println( "Current Node is: " + myIt.getCurrentInt() );    // 2
+         myIt.next();
+         System.out.println( "Current Node is: " + myIt.getCurrentInt() );    // 3
+         myIt.next();
+         System.out.println( "Current Node is: " + myIt.getCurrentInt() );    // 5
+         myIt.next();
+         System.out.println( "Current Node is: " + myIt.getCurrentInt() );    // 7
+         myIt = myList.getIteratorAt( 3 );
+         System.out.println( "Current Node is: " + myIt.getCurrentInt() );    // 7
+         myIt.next();
+         System.out.println( "Current Node is: " + myIt.getCurrentInt() );    // 11
+         myIt.next();
+         System.out.println( "Current Node is: " + myIt.getCurrentInt() );    // 13
+         myIt.next();
+         System.out.println( "Current Node is: " + myIt.getCurrentInt() ); //17
+         System.out.println(val );
+
+      }
+   }
